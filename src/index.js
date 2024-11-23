6501f70364cb430890a3c0391e79e8e9bdd341da
TamariSH <tms.shamugia@gmail.com>
2024-11-23 17:23:19 +0100

    Injected the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -50,7 +50,33 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌍</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15°</strong>
+          </div>
+          <div class="weather-forecast-temperature">9°</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Madrid");
+displayForecast();
